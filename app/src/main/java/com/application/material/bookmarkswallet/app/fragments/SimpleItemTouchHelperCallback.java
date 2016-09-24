3768d292f7dc2e3c48b1aa4ef96893d7e1c57887
davide-ennova <davide.lamanna@ennova.it>
2016-09-24 16:03:23 +0200

    added background and icon on swiped item (only delete action added)

--- app/src/main/java/com/application/material/bookmarkswallet/app/fragments/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/application/material/bookmarkswallet/app/fragments/SimpleItemTouchHelperCallback.java
@@ -1,10 +1,21 @@
 package com.application.material.bookmarkswallet.app.fragments;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.View;
 
+import com.application.material.bookmarkswallet.app.R;
 import com.application.material.bookmarkswallet.app.adapter.BookmarkRecyclerViewAdapter;
+
+import org.joda.time.format.FormatUtils;
 
 public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     private final BookmarkRecyclerViewAdapter adapter;
@@ -39,5 +50,43 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         Log.e("TAG", "Hey swipe");
         adapter.onItemDismiss(viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        //prepare background layout
+        Paint p = new Paint();
+        p.setColor(ContextCompat.getColor(recyclerView.getContext(), R.color.red_400));
+
+        Bitmap icon = BitmapFactory.decodeResource(
+                recyclerView.getContext().getResources(), R.drawable.ic_delete_white_48dp);
+
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            View itemView = viewHolder.itemView;
+            if (dX > 0) {
+                c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
+                        (float) itemView.getBottom(), p);
+                c.drawBitmap(icon,
+                        (float) itemView.getLeft() + pxFromDp(recyclerView.getContext(), 16f),
+                        (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - icon.getHeight())/2,
+                        p);
+            } else {
+                c.drawRect((float) itemView.getRight() + dX, (float) itemView.getTop(),
+                        (float) itemView.getRight(), (float) itemView.getBottom(), p);
+            }
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    }
+
+    /**
+     * TODO mov to utils
+     * @param context
+     * @param dp
+     * @return
+     */
+    public static float pxFromDp(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
     }
 }
